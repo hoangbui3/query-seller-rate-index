d277989206d6669c3a9a61a8de5e728ce264066c
Sergey Bochenkov <bachan@j3qq4.org>
2019-09-01 18:06:19 +0200

    sql query fix, refactoring

---
+++ src/main/java/vn/tiki/discovery/ElasticSearchClient.java
@@ -0,0 +1,191 @@
+package vn.tiki.discovery;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.alias.Alias;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.GetAliasesResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.cluster.metadata.AliasMetaData;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.*;
+import java.util.*;
+
+import vn.tiki.discovery.utils.CommonUtils;
+
+// TODO turn it into a proper singleton
+// TODO change alias AFTER index is created, not BEFORE
+
+public class ElasticSearchClient {
+    public static final String INDEX_NAME = System.getenv("QUERY_SELLER_RATE_ALIAS");
+    public static final String HOST_STR = System.getenv("ELASTIC_SEARCH_HOST");
+    public static final int PORT = Integer.parseInt(System.getenv("ELASTIC_SEARCH_PORT"));
+
+    private static RestHighLevelClient restHighLevelClient;
+
+    public static void insertDataToES(List<String[]> rowsList, String dateSuffix) throws IOException {
+        if (rowsList == null) {
+            return;
+        }
+
+        makeConnection(HOST_STR.split("\t"), PORT);
+
+        try {
+            // starttime of function
+            long startTime = System.currentTimeMillis();
+            List<String> list = getOldAlias(INDEX_NAME);
+            String indexName = INDEX_NAME + "_" + dateSuffix + "_" + (new Random().nextInt(1000));
+            boolean isCreated = createIndex(indexName);
+            if (isCreated) {
+                System.out.println("Create Index " + indexName + " Success!");
+                ESBulkInsert(indexName, rowsList);
+                removeOldIndex(list);
+            }
+            // endtime
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+            System.out.println("Duration: " + duration + " ms");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // closeConnection function to close connection with ES after Insertion.
+        closeConnection();
+    }
+
+    private static synchronized RestHighLevelClient makeConnection(String[] hosts, int port) {
+		if (restHighLevelClient != null) {
+			return restHighLevelClient;
+		}
+
+		HttpHost[] httpHosts = new HttpHost[hosts.length];
+		int count = 0;
+		for (String host : hosts) {
+			httpHosts[count] = new HttpHost(hosts[count].trim(), port);
+			count++;
+		}
+
+        restHighLevelClient = new RestHighLevelClient(RestClient.builder(httpHosts));
+		return restHighLevelClient;
+    }
+
+    private static synchronized void closeConnection() throws IOException {
+        restHighLevelClient.close();
+        restHighLevelClient = null;
+    }
+
+    private static synchronized List<String> getOldAlias(String name) throws IOException {
+        GetAliasesRequest request = new GetAliasesRequest(name);
+        GetAliasesResponse response = restHighLevelClient.indices().getAlias(request, RequestOptions.DEFAULT);
+
+        Map<String, Set<AliasMetaData>> map = response.getAliases();
+        List<String> aliasIndex = new ArrayList<>();
+        for (String index : map.keySet()) {
+            aliasIndex.add(index);
+        }
+        return aliasIndex;
+    }
+
+    private static synchronized void removeOldIndex(List<String> listRemove) throws IOException {
+        if (listRemove != null && !listRemove.isEmpty()) {
+            for (String indexName : listRemove) {
+                System.out.println("Detele index:" + indexName);
+
+                DeleteIndexRequest request = new DeleteIndexRequest(indexName);
+                AcknowledgedResponse deleteIndexResponse = restHighLevelClient.indices().delete(request, RequestOptions.DEFAULT);
+            }
+        }
+    }
+
+    private static synchronized boolean createIndex(String indexName) throws IOException {
+		String indexSettingsAndMappings = CommonUtils.readAll(ElasticSearchClient.class.getClassLoader().getResourceAsStream("query_seller_rate_settings_and_mappings.json"));
+        CreateIndexRequest request = new CreateIndexRequest(indexName);
+		request.source(indexSettingsAndMappings, XContentType.JSON);
+        request.alias(new Alias(INDEX_NAME));
+
+        CreateIndexResponse createIndexResponse = restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+        return createIndexResponse.isAcknowledged();
+    }
+
+    public static synchronized void ESBulkInsert(String indexName, List<String[]> rowsList) throws Exception {
+        // Creating a new BulkRequest object with the name bulkRequest1
+        BulkRequest bulkRequest1 = new BulkRequest();
+
+        // Setting batch value to make requests in batches since our number of rows can be in millions or more than that
+        int batch = 1000;
+        // Conter to check how many rows have been processed
+        int counter = 0;
+        int preCounter = 0;
+        int preID = 0;
+
+        XContentBuilder jb = XContentFactory.jsonBuilder();
+        jb.startObject();
+
+		for (int i = 0; i < rowsList.size(); ++i) {
+			try {
+				String[] fields = rowsList.get(i);
+				int currentID = Integer.valueOf(fields[2]);
+				String query = fields[0];
+				String sellerName = fields[1];
+				String rate = fields[3];
+
+                if (currentID != preID) {
+                    if (preID != 0) {
+                        jb.endArray();
+                        jb.endObject();
+
+                        String id = UUID.randomUUID().toString();
+                        bulkRequest1.add(new IndexRequest(indexName, "seller", id).source(jb));
+                        System.out.println(i + "." + counter + "." + sellerName);
+                        counter++;
+                    }
+
+                    jb = XContentFactory.jsonBuilder();
+                    jb.startObject();
+                    jb.field("seller_name", fields[1]);
+                    jb.field("seller_id", fields[2]);
+                    jb.startArray("queries");
+
+                    preID = currentID;
+                }
+
+                jb.startObject();
+                jb.field("query", query);
+                jb.field("rate", rate);
+                jb.endObject();
+
+                if (i == rowsList.size() - 1) {
+                    jb.endArray();
+                    jb.endObject();
+
+                    String id = UUID.randomUUID().toString();
+                    bulkRequest1.add(new IndexRequest(indexName, "seller", id).source(jb));
+                }
+
+                // checking if counter is divisible by batch, to make us aware of how many batches have been processed
+                // and create a new BulkRequest object
+                if ((counter != preCounter && counter % batch == 0) || i == rowsList.size() - 1) {
+                    BulkResponse bulkResponse = restHighLevelClient.bulk(bulkRequest1, RequestOptions.DEFAULT);
+                    bulkRequest1 = new BulkRequest();
+                    System.out.println("Uploaded: " + counter + " sellers so far");
+                    preCounter = counter;
+                }
+            } catch (Exception ex) {
+                System.out.println("error on row: " + i);
+                ex.printStackTrace();
+                return;
+			}
+		}
+    }
+}
--- src/main/java/vn/tiki/discovery/crawler/BigQueryCrawler.java
+++ src/main/java/vn/tiki/discovery/crawler/BigQueryCrawler.java
@@ -12,123 +12,27 @@ import java.util.Date;
 import java.util.UUID;
 
 public class BigQueryCrawler {
-    private BigQuery bigquery;
+    private final BigQuery bqConnection;
+	private final String bqQueryTemplate;
 
-    public String bigQuery = "WITH\n" +
-            "  click_product AS (\n" +
-            "  SELECT\n" +
-            "    keyword,\n" +
-            "    product_id\n" +
-            "  FROM\n" +
-            "    `tiki-dwh.search_metrics_overall.base_*`\n" +
-            "  WHERE\n" +
-            "    _TABLE_SUFFIX BETWEEN \"fromQuery\"\n" +
-            "    AND \"toQuery\"\n" +
-            "    AND action = 'click on result'\n" +
-            "    AND product_id IS NOT NULL),\n" +
-            "  click_product_seller AS (\n" +
-            "  SELECT\n" +
-            "    click_product.product_id,\n" +
-            "    seller_id,\n" +
-            "    seller_name,\n" +
-            "    keyword\n" +
-            "  FROM\n" +
-            "    `tiki-dwh.dwh.dim_product_full`,\n" +
-            "    click_product\n" +
-            "  WHERE\n" +
-            "    entity_type LIKE 'seller_%'\n" +
-            "    AND click_product.product_id = IFNULL(psuper_id,\n" +
-            "      IFNULL(pmaster_id,\n" +
-            "        product_key)) ),\n" +
-            "  count_total AS (\n" +
-            "  SELECT\n" +
-            "    keyword,\n" +
-            "    count (*) AS total\n" +
-            "  FROM\n" +
-            "    click_product_seller\n" +
-            "  GROUP BY\n" +
-            "    keyword ),\n" +
-            "  count_store_per_keyword AS (\n" +
-            "  SELECT\n" +
-            "    keyword,\n" +
-            "    seller_name,\n" +
-            "    seller_id,\n" +
-            "    COUNT(*) AS count_store\n" +
-            "  FROM\n" +
-            "    click_product_seller\n" +
-            "  GROUP BY\n" +
-            "    keyword,\n" +
-            "    seller_name,\n" +
-            "    seller_id),\n" +
-            "  final AS (\n" +
-            "  SELECT\n" +
-            "    count_total.keyword,\n" +
-            "    seller_name,\n" +
-            "    seller_id,\n" +
-            "    total,\n" +
-            "    count_store,\n" +
-            "    (count_store/total) AS rate\n" +
-            "  FROM\n" +
-            "    count_total,\n" +
-            "    count_store_per_keyword\n" +
-            "  WHERE\n" +
-            "    count_total.keyword = count_store_per_keyword.keyword\n" +
-            "    AND total>100\n" +
-            "    AND seller_name != 'Tiki Trading'\n" +
-            "  ORDER BY\n" +
-            "    rate DESC,\n" +
-            "    total DESC\n" +
-            "  LIMIT\n" +
-            "    16000)\n" +
-            "SELECT\n" +
-            "  replace(keyword,\",\",\" \") as query,\n" +
-            "  replace(seller_name,\",\",\" \") as seller_name,\n" +
-            "  seller_id,\n" +
-            "  rate\n" +
-            "FROM\n" +
-            "  final\n" +
-            "ORDER BY\n" +
-            "  seller_id";
-
-    public BigQueryCrawler(Date date) throws IOException {
-
+    public BigQueryCrawler() throws IOException {
         InputStream fileInputStream = this.getClass().getClassLoader().getResourceAsStream("tiki-search-platform.json");
         GoogleCredentials credentials = GoogleCredentials.fromStream(fileInputStream);
-        bigquery = BigQueryOptions.newBuilder()
+        this.bqConnection = BigQueryOptions.newBuilder()
                 .setCredentials(credentials)
                 .setProjectId("tiki-dwh")
                 .build().getService();
-
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        calendar.add(Calendar.DATE, -1);
-
-        String toQuery = CommonUtils.getStringByDate(calendar.getTime());
-
-        calendar.add(Calendar.DATE, -90);
-        String fromQuery = CommonUtils.getStringByDate(calendar.getTime());
-
-        bigQuery = bigQuery
-                .replace("fromQuery", fromQuery)
-                .replace("toQuery", toQuery);
-
+		this.bqQueryTemplate = CommonUtils.readAll(this.getClass().getClassLoader().getResourceAsStream("query_to_seller_rate.sql"));
     }
 
-
-    public TableResult getBigQueryDataResult() throws InterruptedException, IOException {
-
-        QueryJobConfiguration queryConfig =
-                QueryJobConfiguration.newBuilder(bigQuery)
-                        // Use standard SQL syntax for queries.
-                        // See: https://cloud.google.com/bigquery/sql-reference/
-                        .setUseLegacySql(false)
-                        .build();
-
+    public TableResult getBigQueryDataResult(String dateSuffix) throws InterruptedException, IOException {
+		String bqQueryText = this.bqQueryTemplate.replace("{{ ds_nodash }}", dateSuffix);
+        QueryJobConfiguration queryConfig = QueryJobConfiguration.newBuilder(bqQueryText)
+				.setUseLegacySql(false).build();
 
         JobId jobId = JobId.of(UUID.randomUUID().toString());
         System.out.println("Start Querying Google Big Query\n...");
-        Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+        Job queryJob = bqConnection.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
 
         // Wait for the query to complete.
         queryJob = queryJob.waitFor();
@@ -141,12 +45,8 @@ public class BigQueryCrawler {
             // errors, not just the latest one.
             throw new RuntimeException(queryJob.getStatus().getError().toString());
         }
-        TableResult result = queryJob.getQueryResults();
-        System.out.println("Querying Google Big Query done");
 
-        return result;
-
+        return queryJob.getQueryResults();
     }
-
 }
 
