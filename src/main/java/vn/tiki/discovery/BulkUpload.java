df18fead9f5f09047b041c6924ac00a5b62628b4
Hoang.Bui3 <hoangsaodem@gmail.com>
2019-09-03 15:48:20 +0700

    added multiple day support

--- src/main/java/vn/tiki/discovery/BulkUpload.java
+++ src/main/java/vn/tiki/discovery/BulkUpload.java
@@ -1,5 +1,6 @@
 package vn.tiki.discovery;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -9,34 +10,52 @@ public class BulkUpload {
 
     private static BigQueryClient bigQueryClient;
     private static GoogleStorageClient googleStorageClient;
-	private static ElasticSearchClient elasticSearchClient;
+    private static ElasticSearchClient elasticSearchClient;
 
-	private static final long ONE_DAY_IN_MILLIS = 24 * 60 * 60 * 1000;
-	private static String yesterdayDateSuffix() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-		return sdf.format(new Date(System.currentTimeMillis() - ONE_DAY_IN_MILLIS));
-	}
+    private static final long ONE_DAY_IN_MILLIS = 24 * 60 * 60 * 1000;
+
+    public static String getDateSuffix(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        return sdf.format(date);
+    }
+
+    public static Date parseDate(String format) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        try {
+            return sdf.parse(format);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return new Date();
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
-		bigQueryClient = new BigQueryClient();
-		googleStorageClient = new GoogleStorageClient();
-		elasticSearchClient = new ElasticSearchClient();
+        bigQueryClient = new BigQueryClient();
+        googleStorageClient = new GoogleStorageClient();
+        elasticSearchClient = new ElasticSearchClient();
 
         String job = args.length > 0 ? args[0] : JOB_INSERT_ES;
-        String dateSuffix = args.length > 1 ? args[1] : yesterdayDateSuffix();
+        String dateSuffix = args.length > 1 ? args[1] : getDateSuffix(new Date(System.currentTimeMillis() - ONE_DAY_IN_MILLIS));
 
         if (job.equals(JOB_UPLOAD_GS)) {
-			googleStorageClient.uploadDataToGS(
-				bigQueryClient.getBigQueryDataResult(dateSuffix),
-				"query_seller_rate/" + dateSuffix + ".tsv"
-			);
+
+//			googleStorageClient.uploadDataToGS(
+//					bigQueryClient.getBigQueryDataResult(dateSuffix),
+//					"query_seller_rates/" + dateSuffix + ".tsv"
+//			);
+
+            elasticSearchClient.insertDataToES(
+                    googleStorageClient.getDataFromGS(parseDate(dateSuffix)),
+                    dateSuffix
+            );
         }
 
         if (job.equals(JOB_INSERT_ES)) {
-			elasticSearchClient.insertDataToES(
-				googleStorageClient.getDataFromGS("query_seller_rate/" + dateSuffix + ".tsv"),
-				dateSuffix
-			);
+            elasticSearchClient.insertDataToES(
+                    googleStorageClient.getDataFromGS(parseDate(dateSuffix)),
+                    dateSuffix
+            );
         }
     }
 }
---
+++ src/main/java/vn/tiki/discovery/utils/SellerQueryPair.java
@@ -0,0 +1,10 @@
+package vn.tiki.discovery.utils;
+
+public class SellerQueryPair {
+    public int total;
+    public int sellerCountClick;
+
+    public String sellerID;
+    public String sellerName;
+
+}
