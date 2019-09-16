965285c292299c7fa6c6bf087344d36ef999152e
Hoang.Bui3 <hoangsaodem@gmail.com>
2019-09-16 17:20:27 +0700

    added keyword list

--- src/main/java/vn/tiki/discovery/ElasticSearchClient.java
+++ src/main/java/vn/tiki/discovery/ElasticSearchClient.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 // TODO turn it into a proper singleton
 // TODO change alias AFTER index is created, not BEFORE
@@ -45,11 +46,11 @@ public class ElasticSearchClient {
 
     private static RestHighLevelClient restHighLevelClient;
 
-    public static void insertDataToES(List<String[]> rowsList, String dateSuffix) throws IOException {
+
+    public void insertDataToES(List<String[]> rowsList, String dateSuffix) throws IOException {
         if (rowsList == null) {
             return;
         }
-
         makeConnection(HOST_STR.split("\t"), PORT);
 
         try {
@@ -57,17 +58,23 @@ public class ElasticSearchClient {
             long startTime = System.currentTimeMillis();
             List<String> list = getOldAlias(INDEX_NAME);
             String indexName = INDEX_NAME + "_" + dateSuffix + "_" + (new Random().nextInt(1000));
-            boolean isCreated = createIndex(indexName);
-            if (isCreated) {
-                System.out.println("Create Index " + indexName + " Success!");
-                boolean isInsertSuccess = ESBulkInsert(indexName, rowsList);
-                if (isInsertSuccess) {
-                    createAlias(indexName);
-                } else {
-                    list.clear();
-                    list.add(indexName);
+            int retry = 0;
+            boolean isCreated = false;
+            while (!isCreated && retry < 3) {
+                isCreated = createIndex(indexName);
+                if (isCreated) {
+                    System.out.println("Create Index " + indexName + " Success!");
+                    boolean isInsertSuccess = ESBulkInsert(indexName, rowsList);
+                    if (isInsertSuccess) {
+                        createAlias(indexName);
+                    } else {
+                        list.clear();
+                        list.add(indexName);
+                    }
+                    removeOldIndex(list);
                 }
-                removeOldIndex(list);
+
+                retry++;
             }
             // endtime
             long endTime = System.currentTimeMillis();
@@ -81,7 +88,90 @@ public class ElasticSearchClient {
         closeConnection();
     }
 
-    private static synchronized RestHighLevelClient makeConnection(String[] hosts, int port) {
+
+    private HashMap<String, Integer>[] createWordsFrequencyHashmap(LinkedHashMap<String, SellerQueryPair> queryHashMap,
+                                                                   HashMap<String, Integer>[] frequencyHashMap, boolean isCheckRepeat) {
+
+        if (frequencyHashMap == null) {
+            frequencyHashMap = new LinkedHashMap[3];
+            for (int i = 0; i < frequencyHashMap.length; i++) {
+                frequencyHashMap[i] = new LinkedHashMap<>();
+            }
+        }
+
+        HashMap<String, Integer>[] repeatHashmap = new LinkedHashMap[3];
+        for (int i = 0; i < repeatHashmap.length; i++) {
+            repeatHashmap[i] = new LinkedHashMap<>();
+        }
+        for (String query : queryHashMap.keySet()) {
+            SellerQueryPair pair = queryHashMap.getOrDefault(query, new SellerQueryPair());
+
+            String[] words = query.split("\\s+");
+            for (int i = 0; i < words.length; i++) {
+                int frequency = frequencyHashMap[0].getOrDefault(words[i], 0);
+                frequencyHashMap[0].put(words[i], frequency + pair.sellerCountClick);
+
+                repeatHashmap[0].put(words[i], repeatHashmap[0].getOrDefault(words[i], 0) + 1);
+
+
+                if (words.length > 1 && i < words.length - 1) {
+                    String word2 = words[i] + " " + words[i + 1];
+                    frequency = frequencyHashMap[1].getOrDefault(word2, 0);
+                    frequencyHashMap[1].put(word2, frequency + pair.sellerCountClick);
+
+                    repeatHashmap[1].put(word2, repeatHashmap[1].getOrDefault(word2, 0) + 1);
+
+                }
+
+                if (words.length > 2 && i < words.length - 2) {
+                    String word3 = words[i] + " " + words[i + 1] + " " + words[i + 2];
+                    frequency = frequencyHashMap[2].getOrDefault(word3, 0);
+                    frequencyHashMap[2].put(word3, frequency + pair.sellerCountClick);
+
+                    repeatHashmap[2].put(word3, repeatHashmap[2].getOrDefault(word3, 0) + 1);
+
+                }
+
+            }
+
+        }
+
+        if (isCheckRepeat) {
+            Set<String>[] removeHashmap = new Set[3];
+            for (int i = 0; i < removeHashmap.length; i++) {
+                removeHashmap[i] = new HashSet<>();
+            }
+
+            for (int i = 0; i < repeatHashmap.length; i++) {
+                for (String word : repeatHashmap[i].keySet()) {
+                    int repeat = repeatHashmap[i].getOrDefault(word, 0);
+
+                    if (1f * repeat / queryHashMap.size() <= 0.5) {
+                        removeHashmap[i].add(word);
+                    }
+                }
+            }
+            for (int i = 0; i < repeatHashmap.length; i++) {
+                for (String removeWord : removeHashmap[i]) {
+                    frequencyHashMap[i].remove(removeWord);
+                }
+            }
+        }
+
+        HashMap<String, Integer>[] sortedFrequencyHashMap = new LinkedHashMap[3];
+        for (int i = 0; i < frequencyHashMap.length; i++) {
+            sortedFrequencyHashMap[i] = frequencyHashMap[i]
+                    .entrySet()
+                    .stream()
+                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                    .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2, LinkedHashMap::new));
+        }
+
+        return sortedFrequencyHashMap;
+
+    }
+
+    private synchronized RestHighLevelClient makeConnection(String[] hosts, int port) {
         if (restHighLevelClient != null) {
             return restHighLevelClient;
         }
@@ -98,12 +188,12 @@ public class ElasticSearchClient {
         return restHighLevelClient;
     }
 
-    private static synchronized void closeConnection() throws IOException {
+    private synchronized void closeConnection() throws IOException {
         restHighLevelClient.close();
         restHighLevelClient = null;
     }
 
-    private static synchronized List<String> getOldAlias(String name) throws IOException {
+    private synchronized List<String> getOldAlias(String name) throws IOException {
         GetAliasesRequest request = new GetAliasesRequest(name);
         GetAliasesResponse response = restHighLevelClient.indices().getAlias(request, RequestOptions.DEFAULT);
 
@@ -115,7 +205,7 @@ public class ElasticSearchClient {
         return aliasIndex;
     }
 
-    private static synchronized void removeOldIndex(List<String> listRemove) throws IOException {
+    private synchronized void removeOldIndex(List<String> listRemove) throws IOException {
         if (listRemove != null && !listRemove.isEmpty()) {
             for (String indexName : listRemove) {
                 System.out.println("Detele index:" + indexName);
@@ -126,7 +216,7 @@ public class ElasticSearchClient {
         }
     }
 
-    private static synchronized boolean createIndex(String indexName) throws IOException {
+    private synchronized boolean createIndex(String indexName) throws IOException {
         String indexSettingsAndMappings = CommonUtils.readAll(ElasticSearchClient.class.getClassLoader().getResourceAsStream("query_seller_rate_settings_and_mappings.json"));
         CreateIndexRequest request = new CreateIndexRequest(indexName);
         request.source(indexSettingsAndMappings, XContentType.JSON);
@@ -138,7 +228,7 @@ public class ElasticSearchClient {
         return createIndexResponse.isAcknowledged();
     }
 
-    private static synchronized boolean createAlias(String indexName) throws IOException {
+    private synchronized boolean createAlias(String indexName) throws IOException {
         IndicesAliasesRequest request = new IndicesAliasesRequest();
         IndicesAliasesRequest.AliasActions aliasAction =
                 new IndicesAliasesRequest.AliasActions(IndicesAliasesRequest.AliasActions.Type.ADD)
@@ -153,11 +243,48 @@ public class ElasticSearchClient {
         return indicesAliasesResponse.isAcknowledged();
     }
 
+    public static final float[] FREQUENCY_RATE_THRESHOLD = {0.6f, 0.4f, 0.2f};
+    public static final int[] FREQUENCY_THRESHOLD = {100, 100, 50};
 
-    public static synchronized boolean ESBulkInsert(String indexName, List<String[]> rowsList) throws Exception {
+    private String[] getSellerKeyName(LinkedHashMap<String, SellerQueryPair> queryHashMap, HashMap<String, Integer>[] frequencyHashMap) {
+
+        HashMap<String, Integer>[] innerFrequencyHashmap = createWordsFrequencyHashmap(queryHashMap, null, true);
+        List<String> keyList = new ArrayList<>();
+        float maxKeywordRate = 0f;
+        for (int i = 0; i < 2; i++) {
+            int count = 0;
+            String keyword = null;
+            for (String candidate : innerFrequencyHashmap[i].keySet()) {
+
+                int candidateFrequency = frequencyHashMap[i].getOrDefault(candidate, 0);
+                int innerCandidateFrequency = innerFrequencyHashmap[i].getOrDefault(candidate, 0);
+                if (candidateFrequency > 0) {
+                    float frequencyRate = 1f * innerCandidateFrequency / candidateFrequency;
+                    if (frequencyRate > FREQUENCY_RATE_THRESHOLD[i]
+                            && innerCandidateFrequency > FREQUENCY_THRESHOLD[i]
+                            && frequencyRate * (i + 1) > maxKeywordRate) {
+                        maxKeywordRate = frequencyRate * (i + 1);
+                        keyword = candidate;
+                    }
+                }
+
+                count++;
+                if (count > 3 - i) {
+                    break;
+                }
+            }
+
+            if (keyword != null) {
+                keyList.add(keyword);
+            }
+        }
+
+        return keyList.toArray(new String[keyList.size()]);
+    }
+
+    public synchronized boolean ESBulkInsert(String indexName, List<String[]> rowsList) throws Exception {
         // Creating a new BulkRequest object with the name bulkRequest1
         BulkRequest bulkRequest1 = new BulkRequest();
-
         // Setting batch value to make requests in batches since our number of rows can be in millions or more than that
         int batch = 1000;
         // Conter to check how many rows have been processed
@@ -190,6 +317,19 @@ public class ElasticSearchClient {
             queryHashMap.put(query, pair);
             sellerHashMap.put(sellerID, queryHashMap);
         }
+        HashMap<String, Integer>[] frequencyHashmap = null;
+        for (String sellerID : sellerHashMap.keySet()) {
+            LinkedHashMap<String, SellerQueryPair> queryHashMap = sellerHashMap.getOrDefault(sellerID, new LinkedHashMap<>());
+            frequencyHashmap = createWordsFrequencyHashmap(queryHashMap, frequencyHashmap, false);
+        }
+
+        for (String key : frequencyHashmap[0].keySet()) {
+            if (frequencyHashmap[0].get(key) < 50) {
+                break;
+            }
+            System.out.println(key + " has frequency: " + frequencyHashmap[0].get(key));
+        }
+
 
         int sellerSize = sellerHashMap.size();
         int i = 0;
@@ -198,7 +338,10 @@ public class ElasticSearchClient {
             boolean isLast = false;
             int j = 0;
             LinkedHashMap<String, SellerQueryPair> queryHashMap = sellerHashMap.getOrDefault(sellerID, new LinkedHashMap<>());
+
+            String[] keyList = getSellerKeyName(queryHashMap, frequencyHashmap);
             for (String query : queryHashMap.keySet()) {
+
                 if (i == sellerSize - 1) {
                     if (j == queryHashMap.size() - 1) {
                         isLast = true;
@@ -219,6 +362,7 @@ public class ElasticSearchClient {
 
                             String id = UUID.randomUUID().toString();
                             bulkRequest1.add(new IndexRequest(indexName, "seller", id).source(jb));
+
                             System.out.println(i + "." + counter + "." + sellerName);
                             counter++;
                         }
@@ -227,6 +371,9 @@ public class ElasticSearchClient {
                         jb.startObject();
                         jb.field("seller_name", sellerName);
                         jb.field("seller_id", sellerID);
+                        if (keyList != null && keyList.length > 0) {
+                            jb.field("key_list", keyList);
+                        }
                         jb.startArray("queries");
 
                         preID = currentID;
@@ -238,6 +385,7 @@ public class ElasticSearchClient {
                     jb.endObject();
 
                     if (isLast) {
+
                         jb.endArray();
                         jb.endObject();
 
